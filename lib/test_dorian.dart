c99bcee8b812706eeace70331014dabf7f5c5f87
Dorian Peron <dodo.savenay@gmail.com>
2021-02-07 22:02:26 +0200

    added first Scanner tests

---
+++ lib/test_dorian.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+/*
+/ Scanner widget 
+*/
+class Scanner extends StatefulWidget {
+  @override
+  _ScannerState createState() => _ScannerState();
+}
+
+class _ScannerState extends State<Scanner> {
+  // Hold the value of the scanned barcode once scanned
+  String barcodeData = "None";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: Center(
+        child: Column(
+          children: [
+            Text("Scan Result"),
+            Text('$barcodeData'),
+            RaisedButton(
+              onPressed: scanBarcode,
+              shape: StadiumBorder(),
+              color: Theme.of(context).primaryColor,
+            ),
+          ],
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+      ),
+    );
+  }
+
+  Future<void> scanBarcode() async {
+    try {
+      final scanRes = await FlutterBarcodeScanner.scanBarcode(
+          "#ff0000", "Back", true, ScanMode.BARCODE);
+
+      if (!mounted) return;
+
+      setState(() {
+        this.barcodeData = scanRes;
+      });
+    } on PlatformException {
+      this.barcodeData = "Error";
+    }
+  }
+}
