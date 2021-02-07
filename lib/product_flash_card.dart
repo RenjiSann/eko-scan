47a5caed10c72b831b0fe723836b8b3f65808ef4
Clarisse Blanco <clarisse.blanco@epita.fr>
2021-02-08 00:13:19 +0200

    added scrollable functionnality in memo and product flash card

--- lib/product_flash_card.dart
+++ lib/product_flash_card.dart
@@ -52,141 +52,146 @@ class _FicheProduitState extends State<FicheProduit> {
         ),
         backgroundColor: Theme.of(context).primaryColor,
       ),
-      body: Padding(
-          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 30.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Center(
-                child: Text('${prod.product}',
-                    maxLines: 3, style: Theme.of(context).textTheme.headline1),
-              ),
-              SizedBox(
-                height: 15.0,
-              ),
-              Center(
-                child: CircleAvatar(
-                  backgroundImage: NetworkImage(prod.pic),
-                  radius: 60.0,
-                ),
-              ),
-              Divider(
-                height: 50.0,
-                color: Colors.blueGrey,
-                thickness: 1,
-              ),
-              Row(children: <Widget>[
-                Icon(
-                  Icons.eco_outlined,
-                  color: Colors.greenAccent[700],
-                ),
-                SizedBox(width: 10.0),
-                Text(
-                  'EKO-SCORE : ${prod.score}/10',
-                  maxLines: 3,
-                  style: TextStyle(
-                      color: Colors.greenAccent[700],
-                      letterSpacing: 2.0,
-                      fontSize: 15.0,
-                      fontWeight: FontWeight.w500),
-                ),
-              ]),
-              SizedBox(height: 10.0),
-              Row(
+      body: SingleChildScrollView(
+          child: Padding(
+              padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 30.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Icon(
-                    Icons.autorenew_rounded,
-                    color: Colors.greenAccent[700],
+                  Center(
+                    child: Text('${prod.product}',
+                        maxLines: 3,
+                        style: Theme.of(context).textTheme.headline1),
                   ),
                   SizedBox(
-                    width: 10.0,
+                    height: 15.0,
                   ),
-                  Text('${prod.bin}',
+                  Center(
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(prod.pic),
+                      radius: 60.0,
+                    ),
+                  ),
+                  Divider(
+                    height: 50.0,
+                    color: Colors.blueGrey,
+                    thickness: 1,
+                  ),
+                  Row(children: <Widget>[
+                    Icon(
+                      Icons.eco_outlined,
+                      color: Colors.greenAccent[700],
+                    ),
+                    SizedBox(width: 10.0),
+                    Text(
+                      'EKO-SCORE : ${prod.score}/10',
                       maxLines: 3,
                       style: TextStyle(
+                          color: Colors.greenAccent[700],
+                          letterSpacing: 2.0,
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ]),
+                  SizedBox(height: 10.0),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.autorenew_rounded,
                         color: Colors.greenAccent[700],
-                        letterSpacing: 2.0,
-                      )),
-                ],
-              ),
-              SizedBox(height: 15.0,),
-              RaisedButton(
-                  onPressed: () {
-                    setState(() {
-                      prod.product += " is liked";
-                    });
-                  },
-                  shape: StadiumBorder(),
-                  child:
-                  Icon(Icons.favorite_border_rounded, color: Colors.pink[700], size: 30,)
-              ),
-              Divider(
-                height: 50.0,
-                color: Colors.blueGrey,
-                thickness: 1,
-              ),
-              Text(
-                'Informations',
-                style: Theme.of(context).textTheme.headline1,
-              ),
-              SizedBox(height: 20.0),
-              Row(children: [
-                Icon(
-                  Icons.chevron_right_rounded,
-                  color: Colors.blueGrey,
-                ),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text('Marque : ${prod.brand}',
-                    maxLines: 3,
-                    style: Theme.of(context).textTheme.bodyText1),
-              ]),
-              SizedBox(height: 15.0),
-              Row(children: <Widget>[
-                Icon(
-                  Icons.blur_circular,
-                  color: Colors.blueGrey,
-                ),
-                SizedBox(width: 10.0),
-                Text('${prod.fairtrade}',
-                    maxLines: 3,
-                    style: Theme.of(context).textTheme.bodyText1),
-              ]),
-              SizedBox(
-                height: 15.0,
-              ),
-              Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.flag_outlined,
-                    color: Colors.blueGrey,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text('${prod.bin}',
+                          maxLines: 3,
+                          style: TextStyle(
+                            color: Colors.greenAccent[700],
+                            letterSpacing: 2.0,
+                          )),
+                    ],
                   ),
                   SizedBox(
-                    width: 10.0,
+                    height: 15.0,
+                  ),
+                  RaisedButton(
+                      onPressed: () {
+                        setState(() {
+                          prod.product += " is liked";
+                        });
+                      },
+                      shape: StadiumBorder(),
+                      child: Icon(
+                        Icons.favorite_border_rounded,
+                        color: Colors.pink[700],
+                        size: 30,
+                      )),
+                  Divider(
+                    height: 50.0,
+                    color: Colors.blueGrey,
+                    thickness: 1,
                   ),
                   Text(
-                    'Origine : ${prod.origin}',
-                    maxLines: 3,
-                    style: Theme.of(context).textTheme.bodyText1,
-                  )
+                    'Informations',
+                    style: Theme.of(context).textTheme.headline1,
+                  ),
+                  SizedBox(height: 20.0),
+                  Row(children: [
+                    Icon(
+                      Icons.chevron_right_rounded,
+                      color: Colors.blueGrey,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text('Marque : ${prod.brand}',
+                        maxLines: 3,
+                        style: Theme.of(context).textTheme.bodyText1),
+                  ]),
+                  SizedBox(height: 15.0),
+                  Row(children: <Widget>[
+                    Icon(
+                      Icons.blur_circular,
+                      color: Colors.blueGrey,
+                    ),
+                    SizedBox(width: 10.0),
+                    Text('${prod.fairtrade}',
+                        maxLines: 3,
+                        style: Theme.of(context).textTheme.bodyText1),
+                  ]),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.flag_outlined,
+                        color: Colors.blueGrey,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        'Origine : ${prod.origin}',
+                        maxLines: 3,
+                        style: Theme.of(context).textTheme.bodyText1,
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  Row(children: <Widget>[
+                    Icon(Icons.border_all_rounded, color: Colors.blueGrey),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text('Emballage : ${prod.packaging}',
+                        maxLines: 3,
+                        style: Theme.of(context).textTheme.bodyText1)
+                  ]),
                 ],
-              ),
-              SizedBox(
-                height: 15.0,
-              ),
-              Row(children: <Widget>[
-                Icon(Icons.border_all_rounded, color: Colors.blueGrey),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text('Emballage : ${prod.packaging}',
-                    maxLines: 3,
-                    style: Theme.of(context).textTheme.bodyText1)
-              ]
-              ),
-            ],
-          )),
+              ))),
     );
   }
 }
